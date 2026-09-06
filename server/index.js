040081866f52a6dd3c5be0a2a348feeb5642653b
Radhey Kurtadikar <radhey.kurtadikar@gmail.com>
2026-09-06 22:22:11 +0530

    Refactor CORS configuration in index.js

--- server/index.js
+++ server/index.js
@@ -23,12 +23,12 @@ connectRedis();
 
 app.use(helmet());
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true,
-  })
-);
+app.set("trust proxy", 1);
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
